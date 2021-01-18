55fffcf0134308e9506e9bd332effcfe15f13fec
U-C-S <ucs3721@outlook.com>
2021-01-18 19:45:12 +0530

    Testing the json-parser - v0.2.1

--- src/components/fileInputModal.tsx
+++ src/components/fileInputModal.tsx
@@ -2,10 +2,33 @@ import React from "react";
 import "../styles/fileModal.css";
 
 class FileInputModal extends React.Component {
+  fileInput: any;
+  constructor(props: any) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.fileInput = React.createRef();
+    this.state = {
+      fileAccepted: true,
+    };
+  }
+  handleSubmit(event: any) {
+    event.preventDefault();
+    var x = new FileReader();
+    let z = (x.onload = () => {
+      alert(this.fileInput.current.files[0].name);
+      //@ts-ignore
+      let y = JSON.parse(x.result?.toString().toString());
+      alert(y.kind);
+    });
+    x.readAsText(this.fileInput.current.files[0]);
+  }
   render() {
     return (
       <main>
-        <input type="file" id="file-selector" accept=".json"></input>
+        <form onSubmit={this.handleSubmit}>
+          <input type="file" ref={this.fileInput} accept=".json,.txt"></input>
+          <input type="submit" />
+        </form>
         <h2>PAGE UNDER CONSTRUCTION</h2>
       </main>
     );
