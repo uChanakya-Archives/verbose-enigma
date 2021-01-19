589112f5ee44a4c3f609b3fa135a49405b78d0e7
U-C-S <ucs3721@outlook.com>
2021-01-19 15:45:18 +0530

    v0.2.2

---
+++ src/components/cards.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+class Card extends React.Component {
+  render(){
+    return (
+      <div className="post_card">
+        <h3></h3>
+      </div>
+    )
+  }
+}
+
+export default null;
+/*
+- title
+- subreddit name
+- upvotes and downvotes
+- post URL
+- no of comments
+- archived or not
+
+-permalink
+*/
--- src/components/fileInputModal.tsx
+++ src/components/fileInputModal.tsx
@@ -3,35 +3,50 @@ import "../styles/fileModal.css";
 
 class FileInputModal extends React.Component {
   fileInput: any;
+  fAcc: boolean;
+  JSONfile: object;
   constructor(props: any) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.fAcc = false;
+    this.JSONfile = {};
     this.fileInput = React.createRef();
     this.state = {
-      fileAccepted: true,
+      fileAccepted: false,
     };
   }
-  handleSubmit(event: any) {
-    event.preventDefault();
+  handleSubmit(e: any) {
+    e.preventDefault();
     var x = new FileReader();
-    let z = (x.onload = () => {
-      alert(this.fileInput.current.files[0].name);
-      //@ts-ignore
-      let y = JSON.parse(x.result?.toString().toString());
-      alert(y.kind);
-    });
     x.readAsText(this.fileInput.current.files[0]);
+    x.onload = () => {
+      alert(this.fileInput.current.files[0].name); //@ts-ignore
+      let JSONfileText = x.result?.toString(); //@ts-ignore
+      this.JSONfile = JSON.parse(JSONfileText);
+      this.fAcc = true;
+      this.setState({ fileAccepted: true });
+    };
   }
   render() {
-    return (
-      <main>
-        <form onSubmit={this.handleSubmit}>
-          <input type="file" ref={this.fileInput} accept=".json,.txt"></input>
-          <input type="submit" />
-        </form>
-        <h2>PAGE UNDER CONSTRUCTION</h2>
-      </main>
-    );
+    if (!this.fAcc) {
+      return (
+        <main>
+          <form onSubmit={this.handleSubmit}>
+            <input type="file" ref={this.fileInput} accept=".json,.txt"></input>
+            <input type="submit" />
+          </form>
+          <h2>PAGE UNDER CONSTRUCTION</h2>
+        </main>
+      );
+    } else {
+      //@ts-ignore
+      let meow = this.JSONfile.data.children.length;
+      return (
+        <main>
+          <p>{meow}</p>
+        </main>
+      );
+    }
   }
 }
 
